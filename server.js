dd10121abb1fed209a85241cb5537327d33804b1
Mike Short <mikeshort2014@gmail.com>
2018-12-11 22:34:16 -0700

    Updated skills with Bootstrap

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const pug = require('pug');
+
+app.set('port', process.env.PORT || 5000) 
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+	res.render(process.cwd() + '/views/index.pug');
+})
+
+app.route('/skills').get((req, res) => {
+	res.render(process.cwd() + '/views/skills.pug');
+})
+
+app.route('/projects').get((req, res) => {
+	res.render(process.cwd() + '/views/projects.pug');
+})
+
+app.route('/about-me').get((req, res) => {
+	res.render(process.cwd() + '/views/aboutme.pug');
+})
+
+app.listen(app.get('port'), function () {
+	console.log('Node JS listening on port ' + app.get('port'))
+})
