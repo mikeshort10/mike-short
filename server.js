dd1ba1481aa601a402a4e82146f96ccb922d1938
Mike Short <mikeshort2014@gmail.com>
2019-01-08 23:31:41 -0700

    moved to single page and added in Bootstrap4

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const pug = require('pug');
 
-app.set('port', process.env.PORT || 5000) 
+app.set('port', process.env.PORT || 3000) 
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'pug');
 
