1703bff88f58775d396cde698d627df0b7487a7f
Mike Short <mikeshort2014@gmail.com>
2019-01-09 22:52:49 -0700

    Add Certs

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
-const pug = require('pug');
+const dotenv = require('dotenv').config();
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const helmet = require('helmet')
 
-app.set('port', process.env.PORT || 3000) 
+app.use(helmet())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded( {extended: true}))
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'pug');
 
@@ -20,8 +25,38 @@ app.route('/projects').get((req, res) => {
 
 app.route('/about-me').get((req, res) => {
 	res.render(process.cwd() + '/views/aboutme.pug');
-})
+});
 
-app.listen(app.get('port'), function () {
-	console.log('Node JS listening on port ' + app.get('port'))
+app.route('/contact')
+	.post((req, res) => {
+		var transporter = nodemailer.createTransport({
+		  service: 'gmail',
+		  auth: {
+		    user: process.env.USERNAME,
+		    pass: process.env.PASSWORD
+		  }
+		});
+
+		var mailOptions = {
+		  from: process.env.USERNAME,
+		  to: process.env.USERNAME,
+		  subject: "From Website: " + req.body.subject,
+		  text: req.body.email + "<br>" + req.body.message
+		};
+
+		transporter.sendMail(mailOptions, function(error, info){
+		  if (error) {
+		    console.log(error);
+		  } else {
+		    console.log('Email sent: ' + info.response);
+		  }
+		});
+	})
+	.get((req, res) => {
+		res.render(process.cwd() + '/views/contact.pug');
+	})
+
+
+app.listen(port = process.env.PORT || 3000, function () {
+	console.log('Node JS listening on port ' + port)
 })
