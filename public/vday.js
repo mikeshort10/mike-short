34efa6c8316fca812426f60b2f8829fa0dbe1227
Mike Short <mikeshort2014@gmail.com>
2019-02-15 13:18:02 -0700

    iOS 10 prevent double tap zoom

--- public/vday.js
+++ public/vday.js
@@ -1,3 +1,11 @@
+var lastTouchEnd = 0;
+document.addEventListener('touchend', function (event) {
+  var now = (new Date()).getTime();
+  if (now - lastTouchEnd <= 300) 
+  	event.preventDefault();
+  lastTouchEnd = now;
+}, false);
+
 $(document).ready(() => {
 
 		var take = 0;
