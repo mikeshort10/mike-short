bf1144645ba6e6912d9c15ccf60ce89e880bb274
Mike Short <mikeshort2014@gmail.com>
2019-02-25 11:59:58 -0700

    debug

--- server.js
+++ server.js
@@ -11,11 +11,9 @@ const router 		= express.Router();
 app.use(helmet());
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded( {extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'pug');
-
-
 
 app.get('/', (req, res) => {
 	res.render(process.cwd() + '/views/index.pug');
@@ -30,13 +28,12 @@ app.get('/plus/you', (req, res) => {
 })
 
 app.use('/games', router
-	.use(express.static(path.join(__dirname, 'games', 'build')))
+	.use(express.static(path.join(process.cwd(), 'games', 'build')))
 	.get("/lindsay-granger", (req, res) => {
 			console.log('here');
-			res.sendFile(path.join(__dirname, 'games', 'build', 'index.html'));
-		})
-	)
-
+			res.sendFile(path.join(process.cwd(), 'games', 'build', 'index.html'));
+	})
+)
 
 app.listen(port = process.env.PORT || 5000, function () {
 	console.log('Node JS listening on port ' + port)
